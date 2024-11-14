aa908abb2bca8881bac87e0c99ce577373b17f5c
Dima <117664428+BREADushek18@users.noreply.github.com>
2024-11-14 19:48:19 +0700

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        DatabaseInitializer.initializeDatabase();
+
+        Scanner scanner = new Scanner(System.in);
+
+        try (scanner) {
+            System.out.println("Введите номер задания (от 1 до 8): ");
+            int taskNumber = scanner.nextInt();
+            switch (taskNumber) {
+                case 1:
+                    MusicFetcher.fetchAllMusic();
+                    break;
+                case 2:
+                    MusicFilter.fetchFilteredMusic();
+                    break;
+                default:
+                    System.out.println("Неверный номер задания.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
