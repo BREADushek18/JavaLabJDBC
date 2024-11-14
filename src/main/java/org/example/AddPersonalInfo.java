43601972496d3189f9d7613a31e12b8f562378fc
Dima <117664428+BREADushek18@users.noreply.github.com>
2024-11-14 23:13:02 +0700

    Fix: improved display of information about yourself

--- src/main/java/org/example/AddPersonalInfo.java
+++ src/main/java/org/example/AddPersonalInfo.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class AddPersonalInfo {
@@ -19,17 +20,42 @@ public class AddPersonalInfo {
             statement.executeUpdate(insertVisitor);
 
             String insertBook1 = "INSERT INTO study.books (name, author, " +
-                    "publishing_year, isbn, publisher) VALUES ('Преступление и наказание', " +
-                    "'Достоевский Федор Михайлович', 2024, '9483023859', 'МИФ') " +
-                    "ON CONFLICT (isbn) DO NOTHING;";
+                    "publishing_year, isbn, publisher) VALUES " +
+                    "('Преступление и наказание', 'Достоевский Федор Михайлович', " +
+                    "2024, '9483023859', 'МИФ') ON CONFLICT (isbn) DO NOTHING;";
             String insertBook2 = "INSERT INTO study.books (name, author, " +
-                    "publishing_year, isbn, publisher) VALUES ('Волшебник Изумрудного города', " +
-                    "'Волков Александр Мелентьевич', 2011, '673020058', 'РООССА') " +
-                    "ON CONFLICT (isbn) DO NOTHING;";
+                    "publishing_year, isbn, publisher) VALUES " +
+                    "('Волшебник Изумрудного города', 'Волков Александр Мелентьевич'," +
+                    " 2011, '673020058', 'РООССА') ON CONFLICT (isbn) DO NOTHING;";
             statement.executeUpdate(insertBook1);
             statement.executeUpdate(insertBook2);
 
             System.out.println("Информация добавлена успешно.");
+
+            String selectVisitor = "SELECT * FROM study.visitors WHERE name = " +
+                    "'Dmitriy' AND surname = 'Pshenichny';";
+            ResultSet visitorResultSet = statement.executeQuery(selectVisitor);
+            if (visitorResultSet.next()) {
+                System.out.println("Информация о посетителе:");
+                System.out.println("Имя: " + visitorResultSet.getString("name"));
+                System.out.println("Фамилия: " + visitorResultSet.getString("surname"));
+                System.out.println("Телефон: " + visitorResultSet.getString("phone"));
+                System.out.println("Подписан: " + visitorResultSet.getBoolean("subscribed"));
+            }
+
+            // Вывод информации о любимых книгах
+            String selectBooks = "SELECT * FROM study.books WHERE author = " +
+                    "'Достоевский Федор Михайлович' OR author = 'Волков Александр Мелентьевич';";
+            ResultSet booksResultSet = statement.executeQuery(selectBooks);
+            System.out.println("Любимые книги:");
+            while (booksResultSet.next()) {
+                System.out.println("Название: " + booksResultSet.getString("name"));
+                System.out.println("Автор: " + booksResultSet.getString("author"));
+                System.out.println("Год издания: " + booksResultSet.getInt("publishing_year"));
+                System.out.println("ISBN: " + booksResultSet.getString("isbn"));
+                System.out.println("Издательство: " + booksResultSet.getString("publisher"));
+                System.out.println();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
